e4474d7a542e9fdac8f1d3487c4b56eb93d69924
vivian1891 <asowucore560@gmail.com>
2026-06-12 23:07:43 +0800

    sgqp.cn-2300-6.12

---
+++ sgqp.cn/assets/js/main.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var toggle = document.querySelector(".nav-toggle");
+  var nav = document.querySelector(".site-nav");
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", function () {
+      var open = nav.classList.toggle("is-open");
+      toggle.setAttribute("aria-expanded", open ? "true" : "false");
+    });
+
+    nav.querySelectorAll("a").forEach(function (link) {
+      link.addEventListener("click", function () {
+        nav.classList.remove("is-open");
+        toggle.setAttribute("aria-expanded", "false");
+      });
+    });
+  }
+});
