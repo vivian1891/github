e0c34fe6b7ef0d691456fa6f87a24d094ec1729e
vivian1891 <asowucore560@gmail.com>
2026-06-23 18:31:28 +0800

    shrmzs.cn-6.23-1831

---
+++ shrmzs.cn/assets/js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var toggle = document.querySelector(".nav-toggle");
+  var nav = document.querySelector(".site-nav");
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", function () {
+      var isOpen = nav.classList.toggle("is-open");
+      toggle.setAttribute("aria-expanded", isOpen ? "true" : "false");
+    });
+  }
+});
